6cb19c0616122244b615baf6aca8a864d0da81db
nada bouyahya <b.nada.dev@gmail.com>
2025-09-16 11:01:11 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,12 +12,12 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("enter the item to add:")
+            item = input("Enter the item to add:")
             shopping_list.append(item)
             print(f"{item} has been added to the list.")
         elif choice == '2':
             if item in shopping_list:
-                item = input("enter the item to remove:")
+                item = input("Enter the item to remove:")
                 shopping_list.remove(item)
                 print(f"{item} has been removed from the list.")
             else:
