34efe3ef88e69bf54af0609586f5d2c80e95f6ab
nada bouyahya <b.nada.dev@gmail.com>
2025-09-16 12:39:21 +0100

    add library_system.py

--- oop/main.py
+++ oop/main.py
@@ -1,12 +1,17 @@
-from book_class import Book
+from library_system import Book, EBook, PrintBook, Library
 
 def main():
-    my_book = Book("1984", "George Orwell", 1949)
+    my_library = Library()
 
-    print(my_book)         # Calls __str__
-    print(repr(my_book))   # Calls __repr__
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
-    del my_book            # Calls __del__
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
+
+    my_library.list_books()
 
 if __name__ == "__main__":
     main()
